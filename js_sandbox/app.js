d61f0ed2e58131f791b7820a5c0f8b970cc95cd4
khannaabhishek <khannaabhishek@yahoo.com>
2019-05-25 15:16:46 -0700

    Commit files

---
+++ js_sandbox/app.js
@@ -0,0 +1,16 @@
+console.table({a:1, b:2});
+console.time("Hello");
+  console.log("Running timer");
+  console.log("Running timer");
+  console.log("Running timer");
+  console.log("Running timer");
+  console.log("Running timer");
+  console.log("Running timer");
+console.timeEnd("Hello");
+
+//Dynamically Typed
+
+const name = 'john';
+console.log(typeof name);
+//name=true;
+// console.log(typeof name);
